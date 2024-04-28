2790ffa99780dbe805cf2398a449122cb8b2d16b
Arham Akhtar <arhamakhtar118@gmail.com>
2024-04-28 13:17:36 +0500

    added line of code

---
+++ example.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class base {
+public :
+	int basevalue;
+	base (int v):basevalue (v){}
+	int getvalB() {
+		return basevalue;
+	}
+};
+
+class derived : public base {
+public:
+	string derivedvalue;
+	//derived(int a, string b){}
+	derived(int a, string b) :base(a), derivedvalue(b){}
+	string getvalD() {
+		return derivedvalue;
+	}
+
+};
+int main() {
+	derived d1(343,"yellow");
+	cout << d1.getvalD();
+
+}
